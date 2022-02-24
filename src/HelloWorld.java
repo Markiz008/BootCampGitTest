cc4086868c49223b522baee1828b0e9f12e9f427
Marko <marko.bozidar@gmail.com>
2022-02-24 13:08:47 +0100

    Cetvrti commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Bla bla");
         System.out.println("Treci");
+        System.out.println("drugi bla");
     }
 }
