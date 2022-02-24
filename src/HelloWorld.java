de5a6e73a5ea6993140de256d1f48da7026ae70a
Marko <marko.bozidar@gmail.com>
2022-02-24 13:00:55 +0100

    Treci commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Bla bla");
+        System.out.println("Treci");
     }
 }
